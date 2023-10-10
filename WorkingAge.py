0c484c30b803aa1750db54823dc34a7448dad3a4
jan <jan.cabaces.7e5@itb.cat>
2023-10-10 10:46:10 +0200

    ejercico workinAge

---
+++ WorkingAge.py
@@ -0,0 +1,11 @@
+"""
+Jan Cabaces Batalle
+03/10/2023
+ASIXc M03 UF1 A2
+Descripció:
+"""
+edad=int(input("Introduce tu edad:")
+    if edad>=16:
+        print("Tienes la edad para trabajar")
+    elif edad<=65:
+        print("Ya no tienes edad para trabajar")
