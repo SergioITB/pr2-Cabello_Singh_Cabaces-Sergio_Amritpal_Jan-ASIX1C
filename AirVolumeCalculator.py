59c4a596773d600844111ce22664d353294609ce
sergio.cabello.7e5 <sergio.cabello.7e5@itb.cat>
2023-10-10 10:34:33 +0200

    Sergio

---
+++ AirVolumeCalculator.py
@@ -0,0 +1,6 @@
+try
+ancho = input("Dime el ancho del aula en metros ej:1,50")
+largo = input("Dime el largo del aula en metros ej:3,50")
+alto = input("Dime el alto del aula en metros ej:2,50")
+volumen = ancho*largo*alto
+print (volumen)
