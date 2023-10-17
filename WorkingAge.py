51299633a21cb07aeb9ebd4107e96b54ab6900ff
sergio.cabello.7e5 <sergio.cabello.7e5@itb.cat>
2023-10-17 10:04:17 +0200

    Sergio

--- WorkingAge.py
+++ WorkingAge.py
@@ -1,17 +1,17 @@
 """
-Jan Cabaces Batalle
 03/10/2023
 ASIXc M03 UF1 A2
 Descripció: Revision edad para trabajar.
 """
 try:
     edad=int(input("Introduce tu edad:"))
-    if edad < 16:
-        print("Aun no tienes edad para trabajar")
-    elif edad < 65:
-        print("Puedes trabajar")
-    elif edad >= 65:
-        print("Ya no tienes edad para trabajar")
+
 except:
     print("Introduzca un numero entero")
-
+    edad = int(input("Introduce tu edad:"))
+if edad < 16:
+    print("Aun no tienes edad para trabajar")
+elif edad < 65:
+    print("Puedes trabajar")
+elif edad >= 65:
+    print("Ya no tienes edad para trabajar")
