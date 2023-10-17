9887eb53aad7a3f0da99c457c7e6a2583614b162
Jan <jan.cabaces.7e5@itb.cat>
2023-10-17 10:04:47 +0200

    Encript

--- Encript12345.py
+++ Encript12345.py
@@ -4,16 +4,19 @@ Jan Cabaces Batalle
 ASIXc M03 UF1 A2
 Descripció: Encripta las palabaras por numeros.
 """
-texto = str(input("Introduzca una palabra"))
-print(texto)
-listaLetras = {
-    'a': '1', 'A': '1',
-    'e': '2', 'E': '2',
-    'i': '3', 'I': '3',
-    'o': '4', 'O': '4',
-    'u': '5', 'U': '5',
-}
-for vocal, valor in listaLetras.items():
-    texto = texto.replace(vocal, valor)
+try:
+    texto = str(input("Introduzca una palabra"))
+    print(texto)
+    listaVocales = {
+        'a': '1', 'A': '1',
+        'e': '2', 'E': '2',
+        'i': '3', 'I': '3',
+        'o': '4', 'O': '4',
+        'u': '5', 'U': '5',
+    }
+    for vocal, valor in listaVocales.items():
+        texto = texto.replace(vocal, valor)
 
-print(texto)
+    print(texto)
+except:
+    print("introduce una cadena de texto")
