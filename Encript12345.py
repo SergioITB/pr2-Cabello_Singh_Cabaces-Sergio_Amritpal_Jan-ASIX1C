e18610447f8fe404d1d7357f95261372274c198f
Jan <jan.cabaces.7e5@itb.cat>
2023-10-17 09:52:25 +0200

    Encript

--- Encript12345.py
+++ Encript12345.py
@@ -4,7 +4,16 @@ Jan Cabaces Batalle
 ASIXc M03 UF1 A2
 Descripció: Encripta las palabaras por numeros.
 """
-palabra=int(input("Introduzca una palabra"))
-   print(palabra)
-   listLetras = ["a","e","i","o","u"]
-   listNum = [1,2,3,4,5]
+texto = input("Introduzca una palabra")
+print(texto)
+listaLetras = {
+    'a': '1', 'A': '1',
+    'e': '2', 'E': '2',
+    'i': '3', 'I': '3',
+    'o': '4', 'O': '4',
+    'u': '5', 'U': '5',
+}
+for vocal, valor in listaLetras.items():
+    texto = texto.replace(vocal, valor)
+
+print(texto)
