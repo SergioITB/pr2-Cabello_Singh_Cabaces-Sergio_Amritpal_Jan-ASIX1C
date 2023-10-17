0389c36ff15b6e150738d2170fb0dbd8afb8a935
amritpal.singh.7e5 <amritpal.singh.7e5@itb.cat>
2023-10-17 10:10:17 +0200

    pizza surface

--- HowBigIsMyPizza.py
+++ HowBigIsMyPizza.py
@@ -2,11 +2,10 @@
 Amritpal Singh
 10/10/2023
 How Big is my Pizza
-UwU
 """
 
-import math
-pi = math.pi
-diameter = int(input("Enter the diameter of your pizza:"))
-surface = pi*diameter
-print(surface)
+import math #Imports maths form python's math libray
+pi = math.pi  #defines pi value
+diameter = int(input("Enter the diameter of your pizza:")) #asks user to input data
+surface = pi*diameter #calculates surface by multiplying pi by diameter
+print(surface) #prints the final calculation
--- InRange.py
+++ InRange.py
@@ -1,17 +1,18 @@
 """
-Amritpal Singh
 10/10/2023
 InRange
 """
-first_num= int(input("type a number:"))
-second_num= int(input("type a number:"))
-third_num= int(input("type a number:"))
-fourth_num= int(input("type a number:"))
-fifth_num= int(input("type a number:"))
-first_list = list(range(first_num, second_num))
+
+"""asks user to enter the following numbers for lists"""
+first_num= int(input("type a number from where you want your first list to begin :"))
+second_num= int(input("type a number at which  you want your first list to end:"))
+third_num= int(input("type a number from where you want your second list to begin:"))
+fourth_num= int(input("type a number at which you want your second list to end:"))
+fifth_num= int(input("type a number that you want to find:"))
+first_list = list(range(first_num, second_num))   #declares the list
 second_list = list(range(third_num, fourth_num))
 
-if fifth_num in first_list and second_list:
+if fifth_num in first_list and second_list: #finds if the last number exists in both list
     print("True")
 else:
-    print("False")
+    print("False")  #prints the final answer
